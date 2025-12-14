f06f3e304451e1b8b4bdca9da1f02485b8d389c2
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-12-14 14:15:47 +0530

    update flight-search

--- src/app/pages/flight-search/flight-search.ts
+++ src/app/pages/flight-search/flight-search.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import { FlightService } from '../../services/flight';
 
+
 @Component({
   selector: 'app-flight-search',
-  imports: [],
-  templateUrl: './flight-search.html',
-  styleUrl: './flight-search.css',
+  templateUrl: './flight-search.component.html',
+  styleUrls: ['./flight-search.component.css']
 })
 export class FlightSearchComponent {
-search() {
-  this.flightService.search(this.form).subscribe({
-    next: data => this.flights = data
-  });
-}
 
+
+  form = {
+    source: '',
+    destination: '',
+    date: ''
+  };
+
+
+  flights: any[] = [];
+
+ 
+  constructor(private flightService: FlightService) {}
+
+  search() {
+    this.flightService.search(this.form).subscribe({
+      next: (data: any[]) => {
+        this.flights = data;
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Failed to fetch flights');
+      }
+    });
+  }
 }
